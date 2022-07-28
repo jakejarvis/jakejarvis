cb999362a4ca2ffdbef02955245c3154ac3f565b
Jake Jarvis <jake@jarv.is>
2022-07-27 22:15:31 -0400

    bump react & other deps

--- cli/build.js
+++ cli/build.js
@@ -1,6 +1,5 @@
 import path from "path";
-import { statSync } from "fs";
-import { writeFile } from "fs/promises";
+import fs from "fs/promises";
 import cpy from "cpy";
 import mkdirp from "mkdirp";
 import { transformFileSync } from "@babel/core";
@@ -29,7 +28,7 @@ const { code: babelCode } = transformFileSync(inputFile, {
 await temporaryDirectoryTask(async (tempPath) => {
   // save babel-ified code above to a temporary file -- unfortunately, we can't pipe this code directly into to ncc
   const babelFile = path.join(tempPath, "index.cjs");
-  await writeFile(babelFile, babelCode);
+  await fs.writeFile(babelFile, babelCode);
 
   // hacky warning: ncc needs the node_modules we want to bundle next to the input file, so copy them from here
   await cpy("node_modules/**", path.join(tempPath, "node_modules"));
@@ -47,14 +46,14 @@ await temporaryDirectoryTask(async (tempPath) => {
 
   // write final build to ./dist and make it executable
   await Promise.all([
-    writeFile(path.join(outputDir, "index.cjs"), code, { mode: 0o755 }),
+    fs.writeFile(path.join(outputDir, "index.cjs"), code, { mode: 0o755 }),
     // TODO: figure out if bundling this script from 'open' module is really necessary?
     // https://linux.die.net/man/1/xdg-open
-    writeFile(path.join(outputDir, "xdg-open"), assets["xdg-open"].source, {  mode: 0o755 }),
+    fs.writeFile(path.join(outputDir, "xdg-open"), assets["xdg-open"].source, { mode: 0o755 }),
   ]);
 });
 
 // quick logging of resulting filesizes
 console.log("🎉 Success!");
-console.log(`dist/index.cjs\t${prettyBytes((statSync(path.join(outputDir, "index.cjs"))).size)}`);
-console.log(`dist/xdg-open\t${prettyBytes((statSync(path.join(outputDir, "xdg-open"))).size)}`);
+console.log(`dist/index.cjs\t${prettyBytes((await fs.stat(path.join(outputDir, "index.cjs"))).size)}`);
+console.log(`dist/xdg-open\t${prettyBytes((await fs.stat(path.join(outputDir, "xdg-open"))).size)}`);
